dd9905f9bf14bede8794908071dec988188f8133
Kaushal Kumar <148632391+kaushalkumar94@users.noreply.github.com>
2026-01-26 17:35:31 +0530

    Add files via upload

---
+++ Advance Sql Concepts/temporary table sql.sql
@@ -0,0 +1,28 @@
+-- temporary table
+
+create temporary table  temp_table
+(first_name varchar(50),
+last_name varchar(50),
+fav_movie varchar(50));
+
+select * from temp_table;
+
+insert into temp_table
+values('kk','pk','seven');
+
+
+select * from temp_table;
+
+
+
+
+
+select * from employee_salary;
+
+create temporary table salary_over50k
+select * 
+from employee_salary
+where salary>=50000;
+
+select * 
+from salary_over50k;
