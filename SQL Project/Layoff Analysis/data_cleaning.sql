2d4bb7395ad167e427ff9c7478852de1e60027ad
Kaushal Kumar <148632391+kaushalkumar94@users.noreply.github.com>
2026-01-31 11:56:16 +0530

    Implement data cleaning queries for layoffs data
    Added SQL queries for data cleaning, including removing duplicates and creating staging tables.

--- SQL Project/Layoff Analysis/data_cleaning.sql
+++ SQL Project/Layoff Analysis/data_cleaning.sql
@@ -2,3 +2,81 @@
 
 SELECT *
 FROM layoffs;
+
+-- remove  duplicates
+-- standardize the data 
+-- null values or blank values 
+-- remove any collumn
+
+# WE will make a duplicate of the original table 
+
+CREATE TABLE layoff_staging
+LIKE layoffs;
+
+INSERT layoff_staging
+SELECT *
+FROM layoffs;
+
+SELECT * FROM layoff_staging;
+
+# Removing Duplicates
+
+-- we assigned a row number to the some of the collumns 
+SELECT *,
+ROW_NUMBER() OVER(
+				PARTITION BY company, location,industry,total_laid_off,percentage_laid_off,stage,country,`date`,funds_raised_millions) as row_num
+FROM layoff_staging;
+
+-- now if we can see the rou number comes out to be 1 , now we will run another query if any row_num>1 means that is a duplicate 
+ 
+WITH duplicate_cte as 
+ (
+SELECT *,
+ROW_NUMBER() OVER(
+				PARTITION BY company, location,industry,total_laid_off,percentage_laid_off,stage,country,`date`,funds_raised_millions) as row_num
+FROM layoff_staging
+)
+SELECT * 
+FROM duplicate_cte
+WHERE row_num>1;
+
+
+select * from layoff_staging
+where company ='casper' and company ='hibob';
+
+
+
+
+-- now we have to delete the duplicates which we got in the above query 
+-- note we can not directly delete using the cte (see the query below ) and the response error
+
+
+WITH duplicate_cte as 
+ (
+SELECT *,
+ROW_NUMBER() OVER(
+				PARTITION BY company, location,industry,total_laid_off,percentage_laid_off,stage,country,`date`,funds_raised_millions) as row_num
+FROM layoff_staging
+)
+DELETE  # HERE IF WE TRY DELETE WE GET  --Error Code: 1288. The target table duplicate_cte of the DELETE is not updatable
+
+FROM duplicate_cte
+WHERE row_num>1;
+
+-- we will make a copy of this table again and then delte from it 
+
+CREATE TABLE `layoff_staging2` (
+  `company` text,
+  `location` text,
+  `industry` text,
+  `total_laid_off` int DEFAULT NULL,
+  `percentage_laid_off` text,
+  `date` text,
+  `stage` text,
+  `country` text,
+  `funds_raised_millions` int DEFAULT NULL,
+  `row_num1` int
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+INSERT INTO 
+
